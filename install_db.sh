377dbfb51ea639e89feed9d680d2dc5cfb9c93d8
umaguron <umaguron@gmail.com>
2022-06-24 19:46:43 +0900

    Update install_db.sh

--- install_db.sh
+++ install_db.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-rm -i log.db
+ls log.db &>/dev/null && rm -i log.db 
 
 sqlite3 log.db < log_ddl.sql
+
+ls log.db &>/dev/null || echo failed to create log.db
+ls log.db &>/dev/null && echo new log.db created
 
