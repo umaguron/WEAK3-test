674070ee9debd273f0ef9ba5b0c4dc05da4a9d51
umaguron <umaguron@gmail.com>
2022-06-24 14:31:58 +0900

    Create define_path.py

---
+++ define_path.py
@@ -0,0 +1,29 @@
+"""
+All environment dependent path shoud be defined here.
+"""
+
+"""library location"""
+PYTOUGH_ROOT_PATH = "lib/PyTOUGH-master"
+IAPWS_ROOT_PATH = "lib/iapws"
+
+"""AMESH"""
+# location of AMESH_PROG
+AMESH_DIR = "amesh/Source/"
+# name of executable
+AMESH_PROG = "amesh"
+# Do not change
+AMESH_INPUT_FILENAME = "in"
+# Do not change
+AMESH_SEGMT_FILENAME = "segmt"
+
+"""TOUGH3 executable location"""
+# TOUGH3 
+BIN_DIR = "/home/matsunaga/Tough3/bin/"
+# TOUGH2
+BIN_DIR_T2 = "/home/matsunaga/Tough2/Source_Core/"
+# another TOUGH3 
+BIN_DIR_LOCAL = "/Users/matsunagakousei/sourceCodes/TOUGH3_source/TOUGH3v1.0/Mac_executables/bin"
+
+"""MPICH for parallel execution"""
+MPIEXEC = "/usr/local/bin/mpiexec"
+
