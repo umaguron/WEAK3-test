d8dbf4c0deb87b829797478b73dc4e5fac126b4f
umaguron <umaguron@gmail.com>
2024-03-15 15:15:04 +0900

    作図不良、pathデフォルト変更

--- define_path.py
+++ define_path.py
@@ -3,7 +3,7 @@ All environment dependent path should be defined here.
 """
 
 """library location"""
-PYTOUGH_ROOT_PATH = "lib/PyTOUGH-master"
+PYTOUGH_ROOT_PATH = "lib/PyTOUGH"
 IAPWS_ROOT_PATH = "lib/iapws"
 
 """AMESH"""
@@ -27,11 +27,11 @@ BIN_DIR_LOCAL = "/Users/matsunagakousei/sourceCodes/TOUGH3_source/TOUGH3v1.0/Mac
 """executable name"""
 # filename of executable in BIN_DIR
 EXEC_FILENAME = {
-    'eco2n_v2': "tough3-eco2n_v2",
-    'eos2': "tough3-eos2",
-    "eos3": "tough3-eos3",
-    "eco2n": "tough3-eco2n",
-    "ewasg": "tough3-ewasg",
+    'eco2n_v2': "tough3V1.0-eco2n_v2",
+    'eos2': "tough3V1.0-eos2",
+    "eos3": "tough3V1.0-eos3",
+    "eco2n": "tough3V1.0-eco2n",
+    "ewasg": "tough3V1.0-ewasg",
 }
 # filename of executable in BIN_DIR_T2
 EXEC_FILENAME_T2 = {
